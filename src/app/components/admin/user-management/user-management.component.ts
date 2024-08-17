325853eda0b230b532094de846bdba645cd0b9ad
BALTI Bassem <bacembalti3@gmail.com>
2024-08-17 13:08:30 +0200

    COMMIT 17/08 BBA

--- src/app/components/admin/user-management/user-management.component.ts
+++ src/app/components/admin/user-management/user-management.component.ts
@@ -1,42 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UserService } from '../../../services/user.service';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
   styleUrls: ['./user-management.component.css']
 })
-export class UserManagementComponent implements OnInit {
+export class UserManagementComponent implements OnInit, OnDestroy {
+  userSelected: any;
+  users: any = []
+  private subscriptions: Subscription[] = [];
+  
+  constructor(private userService: UserService, private router: Router) { }
 
-  users:any=[]
-  constructor(private userService:UserService, private router :Router) { }
-
-  //ajouter ondestroy ***BASSEM***
   ngOnInit(): void {
-
-this.userService.getAllUsers().subscribe((data)=>{
-  console.log(data);
-this.users=data
-})
-
-  }
-
-  Edit(id:any){
-this.router.navigate([`editUser/${id}`])
-  }
-
-  Delete(id:any){
-    this.userService.deleteUser(id).subscribe((data) => {
-      console.log('data from be', data)
-    this.userService.getAllUsers().subscribe((data) => {
+    const sub = this.userService.getAllUsers().subscribe((data) => {
+      console.log(data);
       this.users = data
     })
-
-      });
+    this.subscriptions.push(sub);
   }
 
-  setRole(id:number){
+  Edit(id: any) {
     this.router.navigate([`editUser/${id}`])
   }
+
+  Delete(id: any) {
+    const sub = this.userService.deleteUser(id).subscribe((data) => {
+      console.log('data from be', data);
+      this.userService.getAllUsers().subscribe((data) => {
+        this.users = data;
+      });
+    });
+    this.subscriptions.push(sub);
+  }
+
+  setRole(userSelected: any) {
+    this.userSelected = userSelected;
+  }
+  
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+
 }
